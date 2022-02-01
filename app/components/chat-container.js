2d3715bffd80badb23c6dd5c597afaf90fa415ab
Jonathan Matsuda <jonathanmatsuda3@gmail.com>
2022-02-01 00:31:51 -0800

    Created chat container component

---
+++ app/components/chat-container.js
@@ -0,0 +1,15 @@
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+
+export default class ChatContainerComponent extends Component {
+  @tracked
+  messages = [];
+
+  @action
+  async loadMessages() {
+    const { channel: {id, teamId} } = this.args;
+    const resp = await fetch(`/api/teams/${teamId}/channels/${id}/messages`);
+    this.messages = await resp.json();
+  }
+}
